45c7ec370aaf6f0ae8b9e021c6ae3933c67a5f27
zweck <sc@zweck.us>
2023-12-18 08:39:26 -0800

    change background and banner wording

--- src/components/subbanner.tsx
+++ src/components/subbanner.tsx
@@ -6,7 +6,7 @@ import styles from './homepage.module.css';
 const Banner = () => {
     return (
         <div className={styles.themodo}>
-            <h1 className={styles.modotext}> When you buy, Number Go Up!. <br/> When you sell, Number Go Up! <br/> When you do nothing, Number Go Up! </h1> 
+            <h1 className={styles.modotext}> You buy, Number Go Up. <br/> You sell, Number Go Up. <br/> When you do nothing, Number Go Up. </h1> 
             <br/>
             <h2 className={styles.modoayintext}> <Link href="https://www.ayin.app/swap"> Click here and buy NGU now! </Link></h2>
         </div>
