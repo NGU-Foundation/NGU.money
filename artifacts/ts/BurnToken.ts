3272bc4c2d2cb4b56bce7e389579d6522ed7ac0b
cyxe <cyxe@cyxe.io>
2023-12-30 12:28:50 -0800

    update burntoken.ts
    a

--- artifacts/ts/BurnToken.ts
+++ artifacts/ts/BurnToken.ts
@@ -73,7 +73,7 @@ class Factory extends ContractFactory<
   BurnTokenTypes.Fields
 > {
   getInitialFieldsWithDefaultValues() {
-    return this.contract.getInitialFieldsWithDefaultValues();
+    return this.contract.getInitialFieldsWithDefaultValues() as BurnTokenTypes.Fields;
   }
 
   eventIndex = { Burn: 0, Destroy: 1 };
