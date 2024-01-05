eb9df982457be2663d9d9d21888ea22d3ffe1800
John Hopkins <hop@fcim.org>
2024-01-04 22:33:37 -0500

    Updated index page and added the Connect Alephium

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,16 @@
 // Page index.tsx
 import React from 'react'
-import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import Link from 'next/link';
 
 // Alephium Imports
 import { AlephiumConnectButton } from '@alephium/web3-react'
-import { AlephiumWalletProvider } from '@alephium/web3-react';
-import { NodeProvider } from '@alephium/web3';
 
 // Graphics
 import { ThreeTorus } from '@/components/ThreeTorus';
-import { Navbar } from '@/components/NavBar';
+import { Navbar } from '@/components/navbar';
 
 export default function HomePage() {
 
@@ -23,45 +19,55 @@ export default function HomePage() {
     }, []);
 
   return (
-      <div>
+      <div className={styles.indexPage}>
 
         <style>
           @import url(&quot;https://fonts.googleapis.com/css2?family=Tektur&display=swap&quot;);
         </style>
+
         <Navbar></Navbar>
+        <div  style={{paddingLeft: 20, paddingTop: 20}}>
+        <AlephiumConnectButton></AlephiumConnectButton>
+        </div>
+        
+
         <ThreeTorus></ThreeTorus>
-        <br/>
-        <div className={styles.NFTheader}>
-        <h1 className={styles.alphpacaTitleGlow}> NGU.Money&apos;s </h1>
-        <h5 className={styles.pacaDescript}> Number Go Up! </h5>
-        </div>
 
-        <div className={styles.movingText}>
-          <TypeAnimation
-            sequence={[
-                // Same substring at the start will only be typed out once, initially
-                "You buy; Number Go Up!",
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                "You sell; Number Go Up!",
-                1000,
-                'You do nothing; Number Go Up',
-                1000,
-                'Welcome to NGU!',
-                1000,
-            ]}
-            speed={30}
-            repeat={Infinity}
-          />
-        </div>
 
-        <div className={styles.uiHub}>
-          <div className={`${styles.showBorder} ${styles.uiNav}`} style={{height: 300}}>
-            <table className={styles.uiNavItems}>
-              <tr> <button className={styles.buttonSite}> <Link href="/token_create" className={styles.link}> Enter NGU! </Link></button> </tr>
-            </table>
+        <div className={styles.nguMoney}>
+          <br/>
+          <div className={styles.NFTheader}>
+          <h1 className={styles.alphpacaTitleGlow}> NGU.Money&apos;s </h1>
+          <h5 className={styles.pacaDescript}> Number Go Up! </h5>
           </div>
+
+          <div className={styles.movingText}>
+            <TypeAnimation
+              sequence={[
+                  // Same substring at the start will only be typed out once, initially
+                  "You buy; Number Go Up!",
+                  1000, // wait 1s before replacing "Mice" with "Hamsters"
+                  "You sell; Number Go Up!",
+                  1000,
+                  'You do nothing; Number Go Up',
+                  1000,
+                  'Welcome to NGU!',
+                  1000,
+              ]}
+              speed={30}
+              repeat={Infinity}
+            />
+          </div>
+
+          <div className={styles.uiHub}>
+            <div className={`${styles.showBorder} ${styles.uiNav}`} style={{height: 80}}>
+              <table className={styles.uiNavItems}>
+                <tr> <button className={styles.buttonSite}> <Link href="/token_create" className={styles.link}> Enter NGU! </Link></button> </tr>
+              </table>
+            </div>
+          </div>
+          <br/>
         </div>
-        <br/>
       </div>
   )
 }
